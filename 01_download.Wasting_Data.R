a41d8185b6a9332d34b1e35d83efdc3d7ac126f5
Monique Gagnon <moniquegagnon@Monique-Gagnons-MacBook.local>
2021-02-09 19:11:27 -0500

    Read in Wasting Data csv

---
+++ 01_download.Wasting_Data.R
@@ -0,0 +1,8 @@
+#Corinne Vietorisz github hw BI 586
+#Owner: Corinne; Collaborator: Isabel 
+#2/9/21
+
+install.packages("readr")
+library(readr)
+data <- read.csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
+
