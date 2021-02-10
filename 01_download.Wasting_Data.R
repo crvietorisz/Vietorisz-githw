ba0afa42e60357821141946b00631a5444dfeb92
Monique Gagnon <moniquegagnon@Monique-Gagnons-MacBook.local>
2021-02-09 20:01:25 -0500

    push it push it to the git git

--- 01_download.Wasting_Data.R
+++ 01_download.Wasting_Data.R
@@ -7,6 +7,6 @@ library(readr)
 data <- read.csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
 install.packages("ggplot2")
 library(ggplot2)
-boxplot(data$`Average.WD....`~Transect,data=data, main="not sure what to put here",
+boxplot(data$`Average.WD....`~Transect,data=data, main="push it push it to the git git",
         xlab="Transect", ylab="Average wasting disease %")
 
